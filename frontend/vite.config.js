404f83f3047ae6e89bc335e090ffb7ce035f50af
Obiyom Bright <brightobiyom2@gmail.com>
2026-07-28 18:26:31 +0100

    ...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -9,17 +8,8 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-})
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
   server: {
     host: "0.0.0.0",
     port: 5173,
   },
 });
->>>>>>> 7d0a6fe176dbfe4580f9cce794247484c064764b
